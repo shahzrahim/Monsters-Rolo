aa1ba20ce46d70e069d3429da4055d62254fccf7
Shazi Rahim <shazirahim@shazis-mbp.lan>
2019-11-29 16:05:14 -0500

    removed React code and added JSON link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters : [
+        { 
+          name : 'Frankenstein',
+          id:'ascs1' 
+        },
+        { 
+          name : 'Dracula',
+          id:'ascs2' 
+        },
+        { 
+          name : 'Zombie',
+          id:'ascs3' 
+        },
+      ]
+    };
+  }
+
+  // https://jsonplaceholder.typicode.com/users
+  render() {
+    return (
+      <div className="App">
+        {
+         this.state.monsters.map(monster => <h2 key={monster.id}>{monster.name}</h2>) 
+        }
+      </div>
+    );
+  }
+
+}
 export default App;
